443861eac1bad957336195f8def86b3c05c45d50
Ninaad <ninaadlak@gmail.com>
2021-03-14 05:35:38 -0400

    Update login.js
    Added login API call to login-screen.

--- site/scripts/login.js
+++ site/scripts/login.js
@@ -1,12 +1,36 @@
 window.onload = async function() {
     const login_button = document.getElementById("login-button");
-    const username_field = document.getElementById("login-username");
+    const email_field = document.getElementById("login-username");
     const password_field = document.getElementById("login-password");
     const signup_button = document.getElementById("signup-button"); //delete if you don't use this
-
+    
+    const loginUser = (email, password) => {
+        return new Promise(function (resolve, reject) {
+            resolve(fetch(url + "/users/login", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "email": email,
+                    "password": password
+                })
+            }).then((response) => response.json())
+            .then((data) => {
+                return data
+            }))
+        })
+    }
+    
     login_button.onclick = function() {
-        var username = username_field.value;
+        var email = email_field.value;
         var password = password_field.value;
+        loginUser(email, password).then(async (user) => {
+            //Essentially, when you login, you get a new password to do functions
+            token = user.token
+            console.log("LOGIN USER ONLY: ", token, email, password)
+            //Make sure to add the next window location
+        })
     }
 
     signup_button.onclick = function() {
