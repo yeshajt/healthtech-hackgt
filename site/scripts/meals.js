1bd5262092ecea1e66554849533d025ddc3b9c16
Ninaad <ninaadlak@gmail.com>
2021-03-14 07:36:31 -0400

    Update meals.js
    Added method calls for food.

--- site/scripts/meals.js
+++ site/scripts/meals.js
@@ -10,7 +10,97 @@ window.onload = async function() {
     back_button.onclick = function() {
         window.location.href = "/html/tracker.html"
     }
+    
+    const loginUser = (email, password) => {
+    return new Promise(function (resolve, reject) {
+        resolve(fetch(url + "/users/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "email": email,
+                "password": password
+            })
+        }).then((response) => response.json())
+            .then((data) => {
+                return data
+            }))
+        })
+    }
 
+    const addFoodChoices = (token) => {
+        return new Promise(function (resolve, reject) {
+            resolve(fetch(url + "/foods/", {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + token,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "name": name,
+                    "age": age,
+                    "email": email,
+                    "sex": sex,
+                    "password": password,
+                    "restrictions": restrictions
+                })
+            }).then((response) => response.json())
+            .then((data) => {
+                return data
+            }))
+        })
+    }
+    
+    //Have to make all calls to user and updates, etc. here
+    //Adding food choices
+    loginUser(email, password).then(async (user) => {
+    //Essentially, when you login, you get a new password to do functions
+        token = user.token
+        console.log("ADD FOOD: ", token, email, password)
+
+        //Have to make all calls to user and updates, etc. here
+        addFoodChoices(token).then(async (foods) => {
+            //an array of foods
+            //to call a specific food item, use []
+            //to call components in the food items, use Ex: foods[0].protein
+            console.log("FOOD ITEMS: " + foods)
+            //UPDATE STUFF HERE TO PAGE
+            foods.forEach(function (food) {
+                let foodName = food.label
+                let src = food.image
+                let cost = food.price
+                    
+                var img = Document.createElement("img");
+                img.src = src
+                containerWrapper.appendChild(img);
+
+                var bottom_container = Document.createElement("div");
+                bottom_container.className = "bottom-container";
+                containerWrapper.appendChild(bottom_container);
+
+                var info_container = Document.createElement("div");
+                info_container.className = "info-container";
+                bottom_container.appendChild(info_container);
+
+                var name = Document.createElement("div");
+                name.className = "food-name";
+                name.innerHTML = foodName;
+                info_container.appendChild(name);
+
+                var cost = Document.createElement("div");
+                cost.className = "food-cost";
+                cost.innerHTML = cost;
+                info_container.appendChild(cost);
+
+                var checkbox = Document.createElement("input");
+                checkbox.type = "checkbox"
+                checkbox.className = "food-eaten"
+                bottom_container.appendChild(checkbox);
+                
+                meals_wrapper_div.appendChild(container)
+        })
+    })
     // logic:
     // for each recipie from the call:
         // var imgLink = link from api call;
