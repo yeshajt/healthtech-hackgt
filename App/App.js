6d74e450a7ea675c763827ef672abbd5426d59bd
Megan <mdass1001@gmail.com>
2021-03-13 21:00:35 -0500

    Different apps

--- App/App.js
+++ App/App.js
@@ -6,6 +6,53 @@
  * @flow strict-local
  */
 
+ /*
+
+import React from 'react';  
+import { View, Text, Button } from 'react-native';  
+import { createStackNavigator, createAppContainer } from 'react-navigation';  
+  
+class HomeScreen extends React.Component {  
+    render() {  
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>  
+                <Text>Home Screen</Text>  
+                <Button  
+                    title="Go to Profile"  
+                    onPress={() => this.props.navigation.navigate('Profile')}  
+                />  
+            </View>  
+        );
+    }  
+}  
+class ProfileScreen extends React.Component {  
+    render() {  
+        return (  
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>  
+                <Text>Profile Screen</Text>  
+            </View>  
+    );  
+    }  
+}  
+  
+const AppNavigator = createStackNavigator(  
+    {  
+        Home: HomeScreen,  
+        Profile: ProfileScreen  
+    },  
+    {  
+        initialRouteName: "Home"  
+    }  
+);  
+  
+const AppContainer = createAppContainer(AppNavigator);  
+export default class App extends React.Component {  
+    render() {  
+        // return <AppContainer />;  
+    }  
+}  
+*/
+ 
 import React from 'react';
 import type {Node} from 'react';
 import {
@@ -16,6 +63,10 @@ import {
   Text,
   useColorScheme,
   View,
+  TextInput,
+  Button,
+  Alert,
+  ViewPager
 } from 'react-native';
 
 import {
@@ -25,6 +76,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { Divider } from '@material-ui/core';
 
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -54,6 +106,8 @@ const Section = ({children, title}): Node => {
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  
+  const [text, onChangeText] = React.useState('Some text')
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -70,21 +124,37 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-            MEGAN LOOK AT THIS
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <ViewPager initialPage={0}>
+            
+            <View key="0">
+              <Text>Username:</Text>
+              <TextInput
+                      style={styles.textInput}
+                      onChangeText={onChangeText}
+                      placeholder={"Username"}
+                    />
+              <Text>Password:</Text> 
+              <TextInput
+                      style={styles.textInput}
+                      onChangeText={onChangeText}
+                      placeholder={"Password"}
+                    />
+              <Button
+                title="Login"
+                onPress={() => Alert.alert("Button pressed")}
+                />
+
+              <Button
+                title="Sign up"
+                onPress={() => handlePageChange(1)}
+              />
+            </View>
+
+            <View key="1">
+              <Text>yay!!</Text>
+            </View>
+
+          </ViewPager>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -108,6 +178,12 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
+  textInput: {
+    height: 40,
+    width: '50%',
+    borderWidth: 1,
+    margin: 5
+  }
 });
 
 export default App;
