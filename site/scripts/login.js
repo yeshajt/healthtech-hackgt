12b175b33b30e77f9f2bc27dda1fdfdd7e985358
Megan Dass <mdass1001@gmail.com>
2021-03-14 06:08:48 -0400

    sign up

--- site/scripts/login.js
+++ site/scripts/login.js
@@ -2,7 +2,7 @@ window.onload = async function() {
     const login_button = document.getElementById("login-button");
     const username_field = document.getElementById("login-username");
     const password_field = document.getElementById("login-password");
-    const signup_button = document.getElementById("signup-button"); //delete if you don't use this
+    const signup_button = document.getElementById("signup-button");
 
     login_button.onclick = function() {
         var username = username_field.value;
